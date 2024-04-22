a52f942b57da219b997ea1d28a532b19f38c17c9
Arin Paliwal <paliwalarin09@gmail.com>
2024-04-22 11:31:30 +0530

    Fixed Consumers.py

--- home/consumers.py
+++ home/consumers.py
@@ -11,19 +11,14 @@ class TestConsumer(WebsocketConsumer):
     def connect(self):
         self.room_name = "test_consumer"
         self.room_group_name = "test_consumer_group"
-        # async_to_sync(self.channel_layer.group_add)(
-        #     self.room_name, self.room_group_name
-        # )
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_name, self.room_group_name
+        )
         self.accept()
         self.send(text_data=json.dumps({"status": "connected"}))
 
     def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
-        # async_to_sync(self.channel_layer.group_send)(
-        #     self.room_group_name, {"type": "chat_message", "message": message}
-        # )
-        self.send(text_data=json.dumps({"message": message}))
+        pass
 
     def disconnect(self, close_code):
         self.close()
