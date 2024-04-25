dda02c6373248f3b4a51775e5887f7aac59f388d
Arin Paliwal <paliwalarin09@gmail.com>
2024-04-25 09:45:30 +0530

    Error RoomDB - Resolved

--- home/models.py
+++ home/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
 import json
+
+
 class Notification(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     message = models.CharField(max_length=100)
@@ -11,17 +13,17 @@ class Notification(models.Model):
 
     def save(self, *args, **kwargs):
         channels_layer = get_channel_layer()
-        notification_obj=Notification.objects.filter(is_read=False).count()
-        data={
-            "count":notification_obj,
-            "current_notification":self.message
+        notification_obj = Notification.objects.filter(is_read=False).count()
+        data = {
+            "count": notification_obj,
+            "current_notification": self.message
         }
         print(data)
         async_to_sync(channels_layer.group_send)(
             'test_consumer_group',
             {
-                "type":"send_notification",
-                "value":json.dumps(data)
+                "type": "send_notification",
+                "value": data
             }
         )
         super(Notification, self).save(*args, **kwargs)
